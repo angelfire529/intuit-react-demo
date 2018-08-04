adf795a831ce729bce5aaf15cf4c08a0f02e0908
jamie broberg <angelfire529@yahoo.com>
2018-08-03 20:40:33 -0700

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+import 'bootstrap-scss'
+import './app.scss';
+
+class App extends Component {
+	render () {
+		return (
+			<div className="container-fluid">
+			<div className="row">
+				<div className="col-xs-12">
+				<h1 className="header text-center">React App!</h1>
+				</div>
+			</div>
+		</div>
+			)
+
+	}
+}
+
+export default App;
