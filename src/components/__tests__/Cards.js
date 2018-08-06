a3241438bff34965715e61447488c7a54af36f4a
jamie broberg <angelfire529@yahoo.com>
2018-08-05 23:27:39 -0700

    trying some unit testing with jest

--- src/components/__tests__/Cards.js
+++ src/components/__tests__/Cards.js
@@ -1,11 +1,32 @@
 import Cards from '../Card/Cards'
 import React from 'react';
-import renderer from 'react-test-renderer';
+import Enzyme, {shallow, mount} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 
-describe('The Cards Component', () => {
+
+Enzyme.configure({adapter: new Adapter()});
+
+describe('<Cards />', () => {
    it(`it should add new contact`, () => {
-    const component = renderer.create(
-        <Cards />
-    )
+
    }) 
+
+   it('', () => {
+       const newContact = {"firstName": "Billy", "lastName": "Joel", "phone": "5552221123", "email": "jshmoe@test.com", "title": "Director of Finance", "collapse": "false"}
+       const wrapper = mount(<Cards />);
+       wrapper.setState({
+           contacts: [
+            {"firstName": "Joe", "lastName": "Shmoe", "phone": "5552221123", "email": "jshmoe@test.com", "title": "Director of Finance", "collapse": "false"},
+            {"firstName": "Jane", "lastName": "Doe", "phone": "4159874521", "email": "kdoe@test.com", "title": "Sr. Accounts Manager", "collapse": "false"},
+            {"firstName": "Mike", "lastName": "Hannity", "phone": "5322221123", "email": "mhannity@test.com", "title": "Sales", "collapse": "false"},
+           ],
+           openModal: false,
+           isAdd: false,
+       })
+
+       const instance = wrapper.instance();
+
+       instance.addContact(newContact);
+       expect(wrapper.state('contacts')).toHaveLength(4);
+   })
 })
